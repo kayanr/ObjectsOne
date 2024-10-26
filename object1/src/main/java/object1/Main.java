455254e1820fa100b5661e333fd220dba88f69e1
Kayan Reid <kreid@Zipcoders-MBP.fios-router.home>
2024-10-26 06:20:50 -0400

    finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,15 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+
+    Car car1 = new Car("Ford", "F150", 1990, 1000);
+        System.out.println(car1.getMake());
+    Dog dog1 = new Dog("Fluff", 11, "Poodle") ;
+        System.out.println(dog1.getName());
+    Chair chair1 = new Chair("wood", "brown", 4) ;
+        System.out.println(chair1.getMaterial());
+    Person person6 = new Person("Bob", 80) ;
+        System.out.println(person6.name + " is " + person6.age);
+    }
 }
--- object1/src/main/java/object1/Person.java
+++ object1/src/main/java/object1/Person.java
@@ -9,18 +9,34 @@ class Person {
     //The units for height are in "centimeters"
     int height;
     // Constructor
-    public Person(String name, int age, int height) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-        height = 0;
+        this.height = 0;
     }
 
     public static void main(String[] args) {
         // Declare and initialize an object
-        Person person1 = new Person("John", 30, 60);
+        Person person1 = new Person("John", 30);
 
         /* Now you can use the Object `person1` */
         System.out.println("The height is: " + person1.height + "cm");
+
+        Person[] people = new Person[5];
+        Person person2 = new Person("Mary", 35);
+        Person person3 = new Person("Charles", 65);
+        Person person4 = new Person("Susan", 22);
+
+        Person person5 = new Person("Bob", 30);
+
+        people[0] = person1;
+        people[1] = person2;
+        people[2] = person3;
+        people[3] = person4;
+        people[4] = person5;
+
+
+
     }
 }
 
